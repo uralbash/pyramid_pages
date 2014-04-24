ae543d7a086f961024c1d36487b7f27671a1a751
Dmitry <root@uralbash.ru>
2014-04-24 18:00:25 +0600

    ++jqTree

--- sacrud_pages/__init__.py
+++ sacrud_pages/__init__.py
@@ -5,10 +5,28 @@
 # Copyright © 2014 uralbash <root@uralbash.ru>
 #
 # Distributed under terms of the MIT license.
+from pyramid.security import NO_PERMISSION_REQUIRED
+from pyramid.view import view_config
+from models import MPTTPages
+
 __version__ = "0.0.1a"
+
+
+@view_config(route_name='mptt_pages', renderer='/sacrud_pages/base.jinja2',
+             permission=NO_PERMISSION_REQUIRED)
+def index_view(request):
+    pages = request.dbsession.query(MPTTPages)\
+        .filter_by(parent_id=None)
+    context = {'pages': pages}
+    return context
+
 
 def includeme(config):
     config.include('pyramid_jinja2')
     config.add_jinja2_search_path("sacrud_pages:templates")
+
     config.add_static_view('/sacrud_pages_static', 'sacrud_pages:static')
+
+    config.add_route('mptt_pages', '/mptt_pages')
+
     config.scan()
