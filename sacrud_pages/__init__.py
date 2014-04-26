5e0a4c51710b51d26742aee536558d99f1188064
Dmitry <root@uralbash.ru>
2014-04-26 23:17:52 +0600

    view for jqTree

--- sacrud_pages/__init__.py
+++ sacrud_pages/__init__.py
@@ -17,15 +17,31 @@ __version__ = "0.0.1a"
              permission=NO_PERMISSION_REQUIRED)
 def index_view(request):
     pages = request.dbsession.query(MPTTPages)\
-        .filter_by(parent_id=None)
+        .filter_by(parent_id=None).order_by(MPTTPages.id).all()
     context = {'pages': pages}
     return context
+
+
+@view_config(route_name='page_move', renderer='json',
+             permission=NO_PERMISSION_REQUIRED)
+def page_move(request):
+    node = request.matchdict['node']
+    method = request.matchdict['method']
+    left_sibling = request.matchdict['leftsibling']
+    page = request.dbsession.query(MPTTPages).filter_by(id=node).one()
+
+    if method == 'inside':
+        print "foo", page.move_inside(left_sibling)
+    if method == 'after':
+        print "baaaaaaaaar"
+    return ''
 
 
 def includeme(config):
     config.include('pyramid_jinja2')
     config.add_jinja2_search_path("sacrud_pages:templates")
     config.add_static_view('/sacrud_pages_static', 'sacrud_pages:static')
-    config.add_route('mptt_pages', '/mptt_pages')
+    config.add_route('mptt_pages', '/mptt_pages/')
+    config.add_route('page_move', '/move/{node}/{method}/{leftsibling}/')
 
     config.scan()
