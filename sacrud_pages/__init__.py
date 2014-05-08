a79562b27203bb738d6f3ffb6cdca88aee135125
Dmitry <root@uralbash.ru>
2014-05-08 17:25:52 +0600

    little fix

--- sacrud_pages/__init__.py
+++ sacrud_pages/__init__.py
@@ -72,7 +72,7 @@ def page_view(context, request):
             context['page'] = page.redirect
         else:
             return Response(status_code=int(page.redirect_type),
-                            location=page.redirect.get_url())
+                            location='/'+page.redirect.get_url())
     if page.redirect_url:
         return Response(status_code=int(page.redirect_type),
                         location=page.redirect_url)
