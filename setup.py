6e3d2bfb54c4306fa46a9fdb329a6270e66adcf2
Dmitry <root@uralbash.ru>
2014-12-12 17:15:34 +0500

    Hello pyramid_sacrud_pages 0.0.3 version!

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(name):
 
 setup(
     name='pyramid_sacrud_pages',
-    version='0.0.2',
+    version='0.0.3',
     url='http://github.com/ITCase/pyramid_sacrud_pages/',
     author='Svintsov Dmitry',
     author_email='sacrud@uralbash.ru',
