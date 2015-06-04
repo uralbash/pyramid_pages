4d516543882f1449233bf2e37912adf135b1cdaf
Arkadiy Zamaraev <arkadiy@bk.ru>
2015-06-04 19:11:11 +0500

    menu_state_active add to active item

--- pyramid_pages/static/js/main.js
+++ pyramid_pages/static/js/main.js
@@ -1,1 +1,28 @@
+(function($){
 
+  'use strict';
+
+  $(window).load(function(){
+
+    var ACTIVE = 'main-menu-list__item_state_active';
+
+    $('.main-menu-list__item')
+      .hover(function() {
+        var self = $(this);
+        if(self.has('.main-menu-list').length > 0) {
+          self.addClass(ACTIVE);
+          self.siblings('.' + ACTIVE)
+              .removeClass(ACTIVE);
+          self.find('.main-menu-list .main-menu-list__item')
+              .removeClass(ACTIVE);
+        }
+      })
+      .mouseleave(function() {
+        var self = $(this);
+        self.parent()
+            .find('.main-menu-list__item_state_active')
+            .removeClass(ACTIVE);
+      });
+
+  });
+})(jQuery);
