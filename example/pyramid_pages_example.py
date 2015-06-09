8a793ab8ac92836ae26638cefbb418c4e33a2843
Dmitry <root@uralbash.ru>
2015-06-09 16:52:48 +0500

    Added custom resorce #37

--- example/pyramid_pages_example.py
+++ example/pyramid_pages_example.py
@@ -16,13 +16,14 @@ import transaction
 from pyramid.config import Configurator
 from pyramid.events import BeforeRender
 from pyramid.session import SignedCookieSessionFactory
-from sqlalchemy import Column, Integer, engine_from_config
+from sqlalchemy import Column, ForeignKey, Integer, Text, engine_from_config
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import relationship, scoped_session, sessionmaker
 from zope.sqlalchemy import ZopeTransactionExtension
 
 from pyramid_pages.common import Menu
-from pyramid_pages.models import BasePage, MpttPageMixin, FlatPageMixin
+from pyramid_pages.models import BasePage, FlatPageMixin, MpttPageMixin
+from pyramid_pages.routes import PageResource
 
 Base = declarative_base()
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
@@ -33,28 +34,54 @@ CONFIG_PYRAMID_PAGES_DBSESSION = 'pyramid_pages.dbsession'
 
 
 class WebPage(Base, BasePage, MpttPageMixin):
-    __tablename__ = "mptt_pages"
+    __tablename__ = 'mptt_pages'
 
     id = Column('id', Integer, primary_key=True)
 
 
 class NewsPage(Base, FlatPageMixin):
-    __tablename__ = "flat_news"
+    __tablename__ = 'flat_news'
 
     id = Column('id', Integer, primary_key=True)
 
 
-def add_fixture(model, fixtures, session):
-    here = os.path.dirname(os.path.realpath(__file__))
-    file = open(os.path.join(here, fixtures))
-    fixtures = json.loads(file.read())
-    for fixture in fixtures:
-        session.add(model(**fixture))
+class Gallery(Base, BasePage, MpttPageMixin):
+    __tablename__ = 'mptt_gallery'
+
+    id = Column('id', Integer, primary_key=True)
+
+
+class GalleryResource(PageResource):
+    model = Gallery
+    template = 'gallery/index.jinja2'
+
+
+class Photo(Base):
+    __tablename__ = 'photos'
+
+    id = Column('id', Integer, primary_key=True)
+    path = Column('name', Text)
+    gallery_id = Column(Integer, ForeignKey('mptt_gallery.id'))
+    gallery = relationship('Gallery', backref='photos')
+
+
+class Fixtures(object):
+
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, model, fixtures):
+        here = os.path.dirname(os.path.realpath(__file__))
+        file = open(os.path.join(here, fixtures))
+        fixtures = json.loads(file.read())
+        for fixture in fixtures:
+            self.session.add(model(**fixture))
 
 
 def add_global_menu(event):
     event['page_menu'] = Menu(DBSession, WebPage).mptt
     event['news_menu'] = Menu(DBSession, NewsPage).flat
+    event['gallery_menu'] = Menu(DBSession, Gallery).mptt
 
 
 def main(global_settings, **settings):
@@ -74,13 +101,15 @@ def main(global_settings, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
-    add_fixture(WebPage, 'fixtures/pages.json', DBSession)
-    add_fixture(WebPage, 'fixtures/country.json', DBSession)
-    add_fixture(NewsPage, 'fixtures/news.json', DBSession)
+    fixture = Fixtures(DBSession)
+    fixture.add(WebPage, 'fixtures/pages.json')
+    fixture.add(WebPage, 'fixtures/country.json')
+    fixture.add(NewsPage, 'fixtures/news.json')
+    fixture.add(Gallery, 'fixtures/gallery.json')
+    fixture.add(Photo, 'fixtures/photos.json')
     transaction.commit()
 
     # pyramid_pages
-    config.include("pyramid_pages")
     settings[CONFIG_PYRAMID_PAGES_DBSESSION] =\
         settings.get(CONFIG_PYRAMID_PAGES_DBSESSION,
                      DBSession)
@@ -90,8 +119,10 @@ def main(global_settings, **settings):
             {
                 '': WebPage,
                 'pages': WebPage,
-                'news': NewsPage
+                'news': NewsPage,
+                'gallery': GalleryResource,
             })
+    config.include("pyramid_pages")
     config.add_subscriber(add_global_menu, BeforeRender)
     return config.make_wsgi_app()
 
--- pyramid_pages/routes.py
+++ pyramid_pages/routes.py
@@ -23,6 +23,10 @@ CONFIG_DBSESSION = 'pyramid_pages.dbsession'
 
 class PageResource(object):
 
+    renderer = 'pyramid_pages/index.jinja2'
+    view = PageView
+    attr = 'page_with_redirect'
+
     def __init__(self, node, prefix=None):
         self.node = node
         self.prefix = prefix
@@ -40,13 +44,13 @@ class PageResource(object):
     @property
     def __parent__(self):
         if hasattr(self.node, 'parent'):
-            return PageResource(self.node.parent, self.prefix)
+            return self.__class__(self.node.parent, self.prefix)
         elif self.node:
-            return PageResource(None, self.prefix)
+            return self.__class__(None, self.prefix)
         return None
 
     def __getitem__(self, name):
-        children = {str(child.slug or ''): PageResource(child, self.prefix)
+        children = {str(child.slug or ''): self.__class__(child, self.prefix)
                     for child in self.node.children}
         return children[name]
 
@@ -67,7 +71,15 @@ class PageResource(object):
         settings = request.registry.settings
         models = settings[CONFIG_MODELS]
         reversed_models = dict(zip(models.values(), models.keys()))
-        return reversed_models.get(node, None)
+        prefix = reversed_models.get(node, None)
+        if prefix:
+            return prefix
+
+        # search prefix for resource
+        for model in models.values():
+            if hasattr(model, 'model') and model.model == node:
+                return reversed_models.get(model, None)
+        return None
 
 
 def page_factory(request):
@@ -84,12 +96,19 @@ def page_factory(request):
         table = models['']
     else:
         table = models[prefix]
+
+    if hasattr(table, 'model'):
+        resource = table
+        table = table.model
+    else:
+        resource = PageResource
+
     nodes = dbsession.query(table)
     if hasattr(table, 'parent_id'):
         nodes = nodes.filter(or_(table.parent_id.is_(''),
                                  table.parent_id.is_(None),
                                  table.parent.has(table.slug == '/')))
-    return {node.slug: PageResource(node, prefix)
+    return {node.slug: resource(node, prefix)
             for node in nodes if node.slug}
 
 
@@ -105,18 +124,34 @@ def home_page_factory(request):
 
 
 def includeme(config):
+    # Home page factory
+    config.add_route(HOME_PAGE, '/', factory=home_page_factory)
+    config.add_view(PageResource.view,
+                    attr=PageResource.attr,
+                    route_name=HOME_PAGE,
+                    renderer=PageResource.renderer,
+                    context=PageResource,
+                    permission=HOME_PAGE)
+
+    # Default page factory
     config.add_route(PREFIX_PAGE, '/{prefix}*traverse', factory=page_factory)
-    config.add_view(PageView,
-                    attr='page_with_redirect',
+    config.add_view(PageResource.view,
+                    attr=PageResource.attr,
                     route_name=PREFIX_PAGE,
-                    renderer='pyramid_pages/index.jinja2',
+                    renderer=PageResource.renderer,
                     context=PageResource,
                     permission=PREFIX_PAGE)
 
-    config.add_route(HOME_PAGE, '/', factory=home_page_factory)
-    config.add_view(PageView,
-                    attr='page_with_redirect',
-                    route_name=HOME_PAGE,
-                    renderer='pyramid_pages/index.jinja2',
-                    context=PageResource,
-                    permission=HOME_PAGE)
+    # Models from setting factory
+    settings = config.registry.settings
+    models = settings[CONFIG_MODELS]
+    for resource in models.values():
+        if not hasattr(resource, 'model'):
+            continue
+
+        config.add_view(resource.view,
+                        attr=resource.attr,
+                        route_name=PREFIX_PAGE,
+                        renderer=resource.template,
+                        context=resource,
+                        permission=PREFIX_PAGE)
