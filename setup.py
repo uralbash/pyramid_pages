5a5778c08efb7ddcadfa61082d9e0b02520878ca
Dmitry <root@uralbash.ru>
2015-06-18 17:45:55 +0500

    ++version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(name):
 
 setup(
     name='pyramid_pages',
-    version='0.0.1',
+    version='0.0.2.dev1',
     url='http://github.com/ITCase/pyramid_pages/',
     author='Svintsov Dmitry',
     author_email='sacrud@uralbash.ru',
