5f92eb6da8264bc010284aeb0751e6bf52db6f8e
Dmitry <root@uralbash.ru>
2015-07-01 17:17:36 +0500

    revoke

--- pyramid_pages/models.py
+++ pyramid_pages/models.py
@@ -37,7 +37,7 @@ class PageMixin(object):
     description = Column(UnicodeText)
 
     def __repr__(self):
-        return self.name.encode('utf-8') or ''
+        return self.name or ''
 
     def __json__(self, request):
         return self.id
